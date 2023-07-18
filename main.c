1b4ebbeea7a3b7e369b17b7dc781240d6cb1d8f1
Noam Levy <noam5003@gmail.com>
2023-07-18 11:52:01 +0300

    routing stdout to output file

--- main.c
+++ main.c
@@ -22,21 +22,29 @@ void main(int argc, char* argv[])
   int size = data->N;
   int totalSatisfiedCounter = 0;
   int temp;
+  if(freopen("output.txt", "w", stdout) == NULL) // route stdout to output file
+  {
+   fprintf(stderr, "Failed to route stdout to output file. Aborting...\n");
+   deallocateMetadata(data);
+   return;
+  }
+
   for (int i = 0; i <= tCount; i++)
   {
     float t = 2.0 * i / tCount - 1;
     setPointsPositions(data->points, size, t);
     calculateDistances(data->points, size);
     temp = checkProximityCriteria(data->points, size, data->D, data->K, t);
-    if (temp < 0)
+    if (temp < 0) // error while checking proximity criteria. error will be printed by the check function
       break;
     totalSatisfiedCounter += temp;
   }
-  if (temp >= 0) // check if no error occourred
-  {
-    if (totalSatisfiedCounter == 0)
+  
+  // if no error occourred and need to print not found message
+  if (temp >= 0 && totalSatisfiedCounter == 0)
       printf("There were no %d points found for any t.\n", MIN_CRITERIA_POINTS);
-  }
+  
 
+  fclose(stdout); // close output file
   deallocateMetadata(data);
 }
